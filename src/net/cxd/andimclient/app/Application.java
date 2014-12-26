8a2ddeeaad0beb592acf21f3a0f0b3768512ff04
oopp1990 <oop1990@163.com>
2014-12-26 15:51:08 +0800

    1

--- src/net/cxd/andimclient/app/Application.java
+++ src/net/cxd/andimclient/app/Application.java
@@ -1,16 +1,14 @@
 package net.cxd.andimclient.app;
 
 import net.cxd.andimclient.service.LocalService;
+import net.cxd.im.entity.User;
 import net.cxd.im.service.UserHttpService;
 import net.cxd.im.service.impl.UserHttpServiceImpl;
 import android.content.Intent;
-import android.database.SQLException;
-import android.os.UserHandle;
 
-import com.nb82.bean.NIO.im.entity.User;
 import com.nb82.bean.db.CFrameDb;
+import com.nb82.bean.db.sqlite.DbModel;
 import com.nb82.core.AppContextControll;
-import com.nb82.util.DbException;
 import com.nb82.view.bitmap.CBitmap;
 
 public class Application extends AppContextControll {
@@ -30,17 +28,12 @@ public class Application extends AppContextControll {
 	public void onCreate() {
 		cache.put("cbitmap", CBitmap.create(this));
 		CFrameDb cFrameDb = CFrameDb.create(this, "ImClient.db", true);
-		try {
-			cFrameDb.findAllBySql(User.class, " select * from user where id=(select max(id) from user)");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		cache.put("cFrameDb", cFrameDb);
+		
+
 		
 		UserHttpService httpService = new UserHttpServiceImpl();
 		cache.put("httpService", httpService);
-		//TODO   find last login user 
-		
-		
 		
 		Intent intent = new Intent(this, LocalService.class);
 		startService(intent);
--- src/net/cxd/andimclient/view/StartAc.java
+++ src/net/cxd/andimclient/view/StartAc.java
@@ -1,13 +1,17 @@
 package net.cxd.andimclient.view;
 
+import net.cxd.andimclient.R;
 import net.cxd.andimclient.app.Application;
 import net.cxd.andimclient.service.UserService;
 import net.cxd.andimclient.util.TaskId;
+import net.cxd.im.entity.User;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 
+import com.nb82.bean.db.CFrameDb;
+import com.nb82.bean.db.sqlite.DbModel;
 import com.nb82.core.KennerControll;
 import com.nb82.entity.BaseActivity;
 import com.nb82.entity.Task;
@@ -15,21 +19,45 @@ import com.nb82.entity.Task;
 public class StartAc extends BaseActivity{
 	KennerControll kennerControll;
 	Application app;
+	String name;
+	String password;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.im_start);
+		init(null);
+	}
+	
+	@Override
+	public void init(Object object) {
 		app= ((Application)getApplication());
 		kennerControll = (KennerControll) app.cache.get("kennerControll");
+		try {
+			DbModel model = ((CFrameDb)app.cache.get("cFrameDb")).findDbModelBySQL( " select * from user where lastLogin=(select max(lastLogin) from user)");
+			if (model != null) {
+				name = model.get("name").toString();
+				password = model.get("password").toString();
+				setContentView(R.layout.im_start);
+			}else{
+				setContentView(R.layout.im_login);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			setContentView(R.layout.im_login);
+		}
+		if (app.cache.get("user") != null) {
+			login(null);
+		}
 	}
+	
 	@Override
 	public void freash(Object object) {
 		
 		
 		
 	}
+	
 	public void login(View view){
-		String name="";
-		String password="";
 		Task task = new Task(TaskId.login,UserService.class, "login",handler, null);
 		task.params.put("name", name);
 		task.params.put("password", password);
