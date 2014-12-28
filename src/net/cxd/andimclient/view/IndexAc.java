adba9a3f89e0bd41fa42a0e840f22b4ec9ba6cd5
oopp1990 <oopp1990@oopp1990-PC>
2014-12-28 17:16:48 +0800

    b

--- src/net/cxd/andimclient/view/IndexAc.java
+++ src/net/cxd/andimclient/view/IndexAc.java
@@ -1,7 +1,14 @@
 package net.cxd.andimclient.view;
 
+import android.os.Bundle;
+
 import com.nb82.entity.BaseActivity;
 
 public class IndexAc extends BaseActivity{
-	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+	}
 }
